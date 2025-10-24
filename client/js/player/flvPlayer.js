f6db05a30cde227e9324cebe9f18edf8d6a61dd8
ches2010 <41281952+ches2010@users.noreply.github.com>
2025-10-24 10:32:04 +0800

    Create flvPlayer.js

---
+++ client/js/player/flvPlayer.js
@@ -0,0 +1,22 @@
+import { createVideoElement } from './playerManager.js';
+
+export function playFLV(url) {
+  if (!window.flvjs || !flvjs.isSupported()) {
+    throw new Error('浏览器不支持FLV播放');
+  }
+
+  const video = createVideoElement();
+  const player = flvjs.createPlayer({
+    type: 'flv',
+    url: url
+  }, {
+    enableWorker: true,
+    lazyLoadMaxDuration: 3 * 60,
+    autoCleanupSourceBuffer: true
+  });
+
+  player.attachMediaElement(video);
+  player.load();
+  window.currentPlayer = player;
+  return player;
+}
