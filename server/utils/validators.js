4d4ef274cd22780b77f65b0cac4c4ae3798ccdd2
ches2010 <41281952+ches2010@users.noreply.github.com>
2025-10-24 10:28:10 +0800

    Update validators.js

--- server/utils/validators.js
+++ server/utils/validators.js
@@ -1,17 +1,7 @@
-/**
- * 验证直播间ID是否为有效数字
- * @param {string} roomId - 直播间ID
- * @returns {boolean} 是否有效
- */
 function validateRoomId(roomId) {
   return /^\d+$/.test(roomId);
 }
 
-/**
- * 验证URL参数是否存在
- * @param {string} url - 待验证的URL
- * @returns {boolean} 是否有效
- */
 function validateUrl(url) {
   return !!url && (url.startsWith('http://') || url.startsWith('https://'));
 }
